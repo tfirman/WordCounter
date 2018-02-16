afbac9671e4591062d4d56879acbde7395eb87ad
Tim Firman <tkfirman@gmail.com>
2018-02-16 09:46:30 -0800

    adds code to complete first test, adds failing second test

--- WordCounter.Test/ModelTests/WordCountTest.cs
+++ WordCounter.Test/ModelTests/WordCountTest.cs
@@ -10,11 +10,18 @@ namespace WordCounter.Tests
     public class RepeatCounterTest
     {
         [TestMethod]
-        public void IsAnagram_TargetWordDifferent_False()
+        public void WordValid_TargetWordEmpty_Error1()
         {
             RepeatCounter testRepeatCounter = new RepeatCounter();
             string givenString = "The quick brown fox jumps over the lazy dog";
             Assert.AreEqual("Error: please input a word to compare", testRepeatCounter.CountMatches ("", givenString));
+        }
+        [TestMethod]
+        public void WordValid_TargetWordInvalid_Error2()
+        {
+            RepeatCounter testRepeatCounter = new RepeatCounter();
+            string givenString = "I saw a cat";
+            Assert.AreEqual("Error: input was not a word", testRepeatCounter.CountMatches ("a cat", givenString));
         }
     }
 }
--- WordCounter/Models/WordCount.cs
+++ WordCounter/Models/WordCount.cs
@@ -7,9 +7,26 @@ namespace WordCount.Models
 {
     public class RepeatCounter
     {
+        private string _outputPhrase;
+
         public string CountMatches (string checkWord, string given)
         {
-            return "The quick brown fox jumps over the lazy dog";
+            _outputPhrase = "The quick brown fox jumps over the lazy dog";
+            if(!ValidWord(checkWord))
+            {
+                return _outputPhrase;
+            }
+            return _outputPhrase;
+        }
+
+        public bool ValidWord (string word)
+        {
+            if (word == "")
+                {
+                    _outputPhrase = "Error: please input a word to compare";
+                    return false;
+                }
+            return true;
         }
     }
 }
